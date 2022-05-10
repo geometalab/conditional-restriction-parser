5fccd040ce6263a5e3537b57e17f4b297ed9c6ff
Lukas Buchli <lukas.buchli@ost.ch>
2022-05-10 10:13:47 +0200

    Change hspec version to match LTS 19.6 resolver

--- src/Parse/Parser.hs
+++ src/Parse/Parser.hs
@@ -1,1 +1,9 @@
 module Parse.Parser where
+import Parse.AST (Grammar, Expression)
+import Parse.Lib (Parser)
+
+pGrammar :: Parser String Grammar
+pGrammar = undefined
+
+pExpression :: Parser String Expression
+pExpression = undefined
